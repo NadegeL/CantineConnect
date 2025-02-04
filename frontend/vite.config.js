10d6fb0236f3a49269fcec9d3966a5469711bf73
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 <j.romand.digifab@gmail.com>
2025-02-04 11:50:55 +0100

    Switch from tailwindcss to unocss

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,10 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss(),
+    UnoCSS(),
   ],
-  esbuild: {
-    target: 'esnext',
-    platform: 'linux',
-  },
-  server: {
-    host: '0.0.0.0',
-  },
-});
+})
