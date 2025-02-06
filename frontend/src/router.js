48e017ca341d8455493148a3e815b2f442577899
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 <j.romand.digifab@gmail.com>
2025-02-06 15:14:13 +0100

    404 page added

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -5,6 +5,7 @@ import UserList from './components/UserList.vue';
 const routes = [
   { path: '/', component: Home },
   { path: '/users', component: UserList },
+  { path: '/:pathMatch(.*)*', component: () => import('./components/NotFound.vue') }, // Route pour les pages non trouvées
 ];
 
 const router = createRouter({
