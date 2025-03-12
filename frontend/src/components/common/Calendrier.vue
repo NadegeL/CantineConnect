ca54822f560ee4f3db85d179cba475847c92841a
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 <nadege.luthier@gmail.com>
2025-03-12 20:50:49 +0100

    début du calendrier. Erreur de branche

---
+++ frontend/src/components/common/Calendrier.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container mx-auto p-4">
+        <h1 class="text-2xl font-bold mb-4">Gestion Calendrier</h1>
+        <CalendrierCantine />
+    </div>
+</template>
+
+<script>
+import CalendrierCantine from '@/components/common/CalendrierCantine.vue';
+
+export default {
+    components: {
+        CalendrierCantine
+    }
+}
+</script>
